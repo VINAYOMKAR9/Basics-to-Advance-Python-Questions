e5eb1c92c4464cf9744b0e5687eccbe65796f074
VINAYOMKAR9 <vinayomkar9@gmail.com>
2023-03-12 17:41:46 +0530

    Introduction of Keyword in Python

---
+++ 2_keyword.py
@@ -0,0 +1,16 @@
+# There are 35 keywords , 3 special keyword (True , False ,None )
+#
+# We can not assign  any value to the keyword
+#  Ex  :
+# True = 10
+#
+# OUTPUT -->SyntaxError: cannot assign to True
+#
+# TO GET ALL  THE Keyboard:
+#  1.
+ help('keywords')
+
+
+ # 2.
+import keyword
+print(keyword.kwlist)
