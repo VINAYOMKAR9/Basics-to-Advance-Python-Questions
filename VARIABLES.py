09b4104b6d2d7444a893998870d7ef208f91247f
VINAYOMKAR9 <vinayomkar9@gmail.com>
2023-03-13 22:05:13 +0530

    variable

---
+++ VARIABLES.py
@@ -0,0 +1,29 @@
+# 1 . A variables request an entity who's value can change as and when  required .
+
+a = 'Hello'
+print(a)
+
+# 2 . Its a name given to a memory location which holds the actual value
+
+a = 'Hello'
+print(id(a))
+
+# 3. variables can hold object address of different type
+
+a = 'Hello'
+print(type(a))
+b = 10
+print(type(b))
+
+c = [1,2,3]
+print(type(c))
+
+#  4 . All varible should be in lower case :
+
+sachin = 'cricket'
+print(sachin)
+
+# 5 . If there are more than one word in thw variable then we seperate with the underscores.
+
+ram_sita = 'ramayan'
+print(ram_sita)
