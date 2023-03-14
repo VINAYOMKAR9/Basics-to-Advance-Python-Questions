a65d92764da8940f1eb0ffb9d730810d3e81c605
VINAYOMKAR9 <vinayomkar9@gmail.com>
2023-03-14 10:39:36 +0530

    Datatype

---
+++ Datatype.py
@@ -0,0 +1,15 @@
+# Data type represent the type of the data that is stored in a memory location.
+#
+# Variable can hold value ,and every value has  a data type
+#
+# Python is dynamic language , hence we donot define the dataof the variable while declaring it
+
+# the interpreter implicity bind the value with data type
+
+a = 'Hello'
+print(type(a))
+b = 10.55
+print(type(b))
+
+
+
