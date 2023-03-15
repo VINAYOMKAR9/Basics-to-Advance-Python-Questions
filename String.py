a422d2725c7ac33a24b5adaa448828c258c6132b
VINAYOMKAR9 <vinayomkar9@gmail.com>
2023-03-15 21:04:22 +0530

    Introduction Of String

---
+++ String.py
@@ -0,0 +1,14 @@
+# Set or collection of Character
+#
+# Boundary : (' ')  or (" ") or (""" """)
+#
+# Its an immutable data type
+#
+# Length of String :
+#         len(var_name)
+#         len('string')
+
+a = 'hello'
+print(len(a))
+# O/P :- 5
+
