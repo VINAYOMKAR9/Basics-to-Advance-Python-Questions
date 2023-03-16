6a49623de23162f965453d5eff04aaeec7321240
VINAYOMKAR9 <vinayomkar9@gmail.com>
2023-03-16 16:19:32 +0530

    Indexing in String

---
+++ Indexing.py
@@ -0,0 +1,14 @@
+# Process of Extractiong Single Chacter at a time .
+#
+# INDEXING Can be +ve (start with 0 ) and it can be -ve ( -1 ) from the reverse order
+#
+# Syntax :- var_name[INDEX]
+
+a = 'hello'
+print(len(a))
+
+print('Index of a[0] is ',a[0])
+print('Index of a[1] is ',a[1])
+print('Index of a[2] is ',a[2])
+print('Index of a[3] is ',a[3])
+print('Index of a[4] is ',a[4])
